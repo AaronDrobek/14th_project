40870ce67bcff4bdd5cfd34a3365757b593e6aba
Aaron Drobek <adrobek09@gmail.com>
2017-08-14 16:30:35 -0400

    deadline

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const moment = require('moment');
 
 // console.log(chalk.red('Hello world!'));
 // console.log("It is", (chalk.blue( moment('locale').format('LLLL'))));
-console.log("It is", (chalk.blue( moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))));
+console.log("It is", (chalk.blueBright( moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))));
 console.log("It is the", (chalk.magenta( moment().format("DDDo"))),"day of the year.");
-let seconds = function(s){
-  return s * 1.1574074074074;
-  console.log("It is", (chalk.blue( moment().format(seconds(s)))),"seconds into the day.");
-};
+
+  let seconds = (moment().format("s"));
+  let minutes = (moment().format("m"));
+  let hours = (moment().format("k"));
+  // console.log(seconds);
+  // console.log(minutes);
+  // console.log(hours);
+  function total(){
+    // console.log( (hours * 60) + (minutes * 60) + seconds);
+  return  (hours * 60) + (minutes * 60) + seconds;
+  }
+  console.log("It is " + chalk.cyan(total()) + " seconds into the day.");
+
 if ( moment().isDST()){
 console.log("It", chalk.green("is"), "during Daylight Savings Time.");
 }else{
